f4ca3f6e0adab5f4077fe20d46f08a7f890d9496
Ksenia <130775320+Xen1usss@users.noreply.github.com>
2025-02-22 19:04:46 +0300

    clean up file icon

--- app/src/main/java/ks/connecttooffice10/ui/screen/FileItem.kt
+++ app/src/main/java/ks/connecttooffice10/ui/screen/FileItem.kt
@@ -1,42 +1,38 @@
 package ks.connecttooffice10.ui.screen
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ks.connecttooffice10.R
 import ks.connecttooffice10.ui.theme.ConnectToOffice10Theme
-import ks.connecttooffice10.ui.theme.Purple40
 
 @Composable
-fun FileItem(fileName: String) {
+fun FileItem(
+    fileName: String,
+    listItemType: ItemsType
+) {
     Row(
         modifier = Modifier
             .padding(16.dp)
             .fillMaxWidth(),
     ) {
-        FileIcon()
+        FileOrRoomOrFolderListItemIcon(listItemType)
         Text(
             modifier = Modifier
                 .padding(start = 16.dp)
@@ -45,89 +41,43 @@ fun FileItem(fileName: String) {
             style = MaterialTheme.typography.bodySmall
         )
     }
-
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 @Composable
-@Preview(showBackground = true)
-private fun FileIcon() {
-    Icon(
-        tint = Color.White,
-        painter = painterResource(R.drawable.ic_folder_open),
-        contentDescription = null,
-    )
-}
-
-
-
-@Composable
-@Preview(showBackground = true)
-fun Kruzhochek() {
+private fun FileOrRoomOrFolderListItemIcon(
+    listItemType: ItemsType
+) {
     Box(
         modifier = Modifier
             .clip(CircleShape)
             .size(32.dp, 32.dp)
-            .background(MaterialTheme.colorScheme.primary)
+            .background(MaterialTheme.colorScheme.primary),
+        contentAlignment = Alignment.Center
     ) {
-
+        Icon(
+            tint = Color.White,
+            painter = painterResource(listItemType.iconResId),
+            contentDescription = null,
+        )
     }
 }
-
 
 @Composable
 @Preview(showBackground = true)
-fun FolderIcon() {
-    Box(
-        modifier = Modifier,
-        contentAlignment = Alignment.Center
-    ) {
-        Kruzhochek()
-        FileIcon()
+private fun PreviewFileOrRoomOrFolderListItemIcon() {
+    ConnectToOffice10Theme {
+        FileOrRoomOrFolderListItemIcon(ItemsType.FILE)
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
 @Composable
-//@Preview(showBackground = true)
+@Preview(showBackground = true)
 fun PreviewFile() {
     ConnectToOffice10Theme {
-        FileItem("Test.docx")
+        FileItem("Test.docx", ItemsType.FILE)
     }
+}
+
+enum class ItemsType(@DrawableRes val iconResId: Int) {
+    FILE(R.drawable.ic_folder_open), FOLDER(R.drawable.ic_folder_open), ROOM(R.drawable.ic_folder_open)
 }
